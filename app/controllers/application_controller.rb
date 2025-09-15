352345101ada05b2b523de05844725dbee904a44
sugita-daiki <footballista.2016@gmail.com>
2025-09-15 10:34:23 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-end
 
-private
+  private
 
-def basic_auth
-  authenticate_or_request_with_http_basic do |username, password|
-    username == 'admin' && password == '2222'
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
   end
 end
