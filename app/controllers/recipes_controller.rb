a0c7653ae1db9ab32a88859c5b5f05a8715cb7cd
sugita-daiki <footballista.2016@gmail.com>
2025-09-19 15:34:05 +0900

    投稿機能実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,24 @@
 class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
-    @recipes = [] if @recipes.nil?
   end
 
   def new
     @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+    if @recipe.save
+      redirect_to recipes_path, notice: 'レシピが正常に投稿されました！'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description)
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :description, presence: true
+
+  scope :recent, -> { order(created_at: :desc) }
 end
